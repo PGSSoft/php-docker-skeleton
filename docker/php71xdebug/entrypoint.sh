681b9e5f9f60a830a1d6e252bd859b630d61628c
Michal Kruczek <michal@clearcode.eu>
2017-04-29 13:15:11 +0200

    Improved docker.sh script.
    Got rid of advanced case statement.
    Removed useless sleeps.
    Updated README.md

--- docker/php71xdebug/entrypoint.sh
+++ docker/php71xdebug/entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 
-echo "PROJECT_XDEBUG_ENABLED = ${PROJECT_ENABLED_XDEBUG}"
+echo "PROJECT_XDEBUG_ENABLED = $PROJECT_XDEBUG_ENABLED"
 if [ $PROJECT_XDEBUG_ENABLED == false ]; then
     echo -e "Disabling xdebug\n";
     echo ';zend_extension=xdebug.so' > /usr/local/etc/php/conf.d/docker-php-pecl-xdebug.ini
 fi
 
-sleep 10;
 if [ "$GITHUB_TOKEN" != "" ]; then composer config -g github-oauth.github.com ${GITHUB_TOKEN}; fi
 composer install --prefer-dist --no-interaction --no-progress --ignore-platform-reqs
 phing
